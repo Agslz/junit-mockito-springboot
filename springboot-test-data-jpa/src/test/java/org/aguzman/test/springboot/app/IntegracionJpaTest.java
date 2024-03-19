64267cb3f37311379afb91dee9f4501c80850b66
Agustín Schulze <agsp3rs3o@gmail.com>
2024-03-19 13:52:04 -0300

    Big Update - Chapter 86

--- springboot-test-data-jpa/src/test/java/org/aguzman/test/springboot/app/IntegracionJpaTest.java
+++ springboot-test-data-jpa/src/test/java/org/aguzman/test/springboot/app/IntegracionJpaTest.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-public class IntegracionJpaTest {
+public class  IntegracionJpaTest {
     @Autowired
     CuentaRepository cuentaRepository;
 
